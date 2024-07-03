931fad1bab6280380e0c975d1567992f4c19dbdf
Matthew Fawcett <107450303+otrai@users.noreply.github.com>
2024-07-02 19:04:21 -0700

    Created the displayInfo method which accepts Bunny objects and outputs their name and age.

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,21 @@ public class Main {
         Bunny myBunny = makeBunny("Easter", 16);
         System.out.println(myBunny.name);
         System.out.println(myBunny.age);
-
+        Bunny myBunny2 = makeBunny("Trix", 6);
+        displayInfo(myBunny);
+        displayInfo(myBunny2);
 }
 
-public static Bunny makeBunny (String name, int age) {
-    Bunny newBunny = new Bunny();
-    newBunny.age = age;
-    newBunny.name = name;
-    return newBunny;
+    public static Bunny makeBunny(String name, int age) {
+        Bunny newBunny = new Bunny();
+        newBunny.age = age;
+        newBunny.name = name;
+        return newBunny;
     }
+
+    public static void displayInfo(Bunny bunny) {
+        System.out.println(bunny.name + " is a " + bunny.age + " year old bunny.");
+    }
+
+
 }
